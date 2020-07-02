29eac8ac62fcd68d31ace6854b5ea2fde44c0d38
Solomon Mori <smori@jahnelgroup.com>
2020-07-02 14:18:52 -0400

    adds connect4 and schnapsen to lobby implementation and sets the LobbyWrapper component as the application entry point

--- src/App.lobby.js
+++ src/App.lobby.js
@@ -5,17 +5,30 @@ import { Lobby } from 'boardgame.io/react'
 import { TicTacToe } from './tic-tac-toe/tic-tac-toe-game'
 import { TicTacToeBoard } from './tic-tac-toe/TicTacToeBoard'
 
+import { ConnectFourBoard } from './connect-four/ConnectFourBoard'
+import { ConnectFour } from './connect-four/connect-four-game'
+
+import {SchnapsenBoard} from './schnapsen/SchnapsenBoard'
+import {Schnapsen} from './schnapsen/schnapsen-game'
+
+
 
 let components =  [
       {
         board: TicTacToeBoard,
         game: { ...TicTacToe, minPlayers: 1, maxPlayers: 2 },
       },
-      { board: 'Board2', game: { name: 'GameName2' } },
-      {
-        board: 'Board3',
-        game: { name: 'GameName3', minPlayers: 1, maxPlayers: 1 },
+      { 
+      	board: ConnectFourBoard, game: { ...ConnectFour, minPlayers: 1, maxPlayers: 2 } 
       },
+      {
+        board: SchnapsenBoard,
+        game: { ...Schnapsen, minPlayers: 1, maxPlayers: 2 },
+      },
+      // {
+      //   board: 'Board3',
+      //   game: { name: 'GameName3', minPlayers: 1, maxPlayers: 1 },
+      // },
     ];
 
 
